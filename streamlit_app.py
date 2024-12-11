9600dca7f77d15aaeb1c61ae606e5d64e572ea7c
Vaelin6901 <108535333+Vaelin6901@users.noreply.github.com>
2024-12-11 17:23:29 +0100

    Update streamlit_app.py
    return v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
-pip install -r streamlit-authenticator
-pip install -r streamlit-option-menu
-
-
 import streamlit as st
 from streamlit_authenticator import Authenticate
 from streamlit_option_menu import option_menu
